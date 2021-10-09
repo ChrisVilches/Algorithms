b803818a78e14ab9d892fa74bae845c8fb3491e2
Felipe Vilches <felovilches@gmail.com>
2021-10-10 00:38:31 +0900

    azulejos, full refactor

--- kattis/azulejos.cpp
+++ kattis/azulejos.cpp
@@ -1,70 +1,56 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-struct Tile {
-  int price, height, id;
-  bool operator <(const Tile& t) const{
-    if(height == t.height){
-      return id < t.id;
-    }
-    return height < t.height;
-  }
-};
+typedef pair<int, int> pii;
 
 int n;
 
-void read_tiles(map<int, set<Tile>> &row){
-  vector<Tile> tiles;
+void read_tiles(map<int, set<pii>> &row){
+  vector<pii> tiles; // ID, price
   for(int i=0; i<n; i++){
-    Tile t;
-    t.id = i + 1;
-    scanf("%d", &t.price);
+    pii t;
+    t.first = i + 1;
+    scanf("%d", &t.second);
     tiles.push_back(t);
   }
   for(int i=0; i<n; i++){
-    scanf("%d", &tiles[i].height);
-    row[tiles[i].price].insert(tiles[i]);
+    int height;
+    scanf("%d", &height);
+    row[tiles[i].second].insert(make_pair(height, tiles[i].first));
   }
 }
 
 int main(){
   cin >> n;
-  map<int, set<Tile>> back, front;
+  map<int, set<pii>> back, front;
 
   read_tiles(back);
   read_tiles(front);
 
-  vector<Tile> back_result, front_result;
+  vector<pii> back_result, front_result;
 
   for(int i=0; i<n; i++){
-    set<Tile>& back_set = back.begin()->second;
-    set<Tile>& front_set = front.begin()->second;
-    set<Tile>::iterator add_to_back, add_to_front;
+    set<pii>& back_set = back.begin()->second;
+    set<pii>& front_set = front.begin()->second;
+    set<pii>::iterator add_to_back, add_to_front;
 
     if(back_set.size() >= front_set.size()){
       add_to_front = front_set.begin();
-
-      // TODO: This has to be done because of some issues with the comparator (can be improved/simplified).
-      Tile t = *add_to_front;
-      t.id = INT_MAX;
-      add_to_back = back_set.upper_bound(t);
+      add_to_back = back_set.upper_bound(make_pair(add_to_front->first, INT_MAX));
+      if(add_to_back == back_set.end()){
+        cout << "impossible" << endl;
+        return 0;
+      }
     } else {
       add_to_back = back_set.begin();
-      Tile t = *add_to_back;
-      t.id = -1;
-      add_to_front = front_set.lower_bound(t);
+      add_to_front = front_set.lower_bound(make_pair(add_to_back->first, 0));
 
       if(add_to_front == front_set.begin()){
         cout << "impossible" << endl;
+        return 0;
       }
 
       --add_to_front;
-    }
-
-    if((add_to_back == back_set.end()) || (add_to_front == front_set.end())){
-      cout << "impossible" << endl;
-      return 0;
     }
 
     back_result.push_back(*add_to_back);
@@ -78,13 +64,13 @@ int main(){
   }
 
   for(int i=0; i<n; i++){
-    printf("%d ", back_result[i].id);
+    printf("%d ", back_result[i].second);
   }
 
   printf("\n");
 
   for(int i=0; i<n; i++){
-    printf("%d ", front_result[i].id);
+    printf("%d ", front_result[i].second);
   }
 
   printf("\n");
