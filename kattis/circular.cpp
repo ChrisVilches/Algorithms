5c4e65ff63543f20307429178906773a936dbb8b
Felipe Vilches <felovilches@gmail.com>
2021-10-10 06:30:17 +0900

    circular DONE (no refactor yet)

--- kattis/circular.cpp
+++ kattis/circular.cpp
@@ -16,7 +16,7 @@ int closed_values[MAX_N] = {0};
 
 int main(){
   cin >> n;
-  set<int> genes;
+  unordered_set<int> genes;
 
   for(int i=0; i<n; i++){
     scanf(" %c%d ", &c, &num);
@@ -24,61 +24,69 @@ int main(){
     gen_type[i] = num;
   }
 
-  
-
-  int open = 0;
   for(int pos=0; pos<n; pos++){
     int i = gen_type[pos];
     int m = marker[pos];
+    if(m == S){
+      gen_status[i]++;
+    } else if(m == E) {
+      gen_status[i]--;
+    }
+  }
+  for(int pos=0; pos<n; pos++){
+    int i = gen_type[pos];
+    if(gen_status[i] == 0)
+      genes.insert(i);
+  }
+  memset(gen_status, 0, sizeof gen_status);
 
-    if(open < 0) throw runtime_error("open < 0");
+  //int open = 0;
+  unordered_set<int> open_genes;
+  for(int pos=0; pos<n; pos++){
+    int i = gen_type[pos];
+    int m = marker[pos];
+    if(genes.find(i) == genes.end()) continue;
 
     if(m == S){
+      open_genes.insert(i);
       if(gen_status[i] == 0){
-        
-        genes.insert(i);
-        open++;
+        open_genes.insert(i);
       }
       gen_status[i]++;
     } else if(m == E) {
       if(gen_status[i] > 0){
         gen_status[i]--;
         if(gen_status[i] == 0){
-          open--;
+          open_genes.erase(i);
+          if(genes.find(i) == genes.end()) throw runtime_error("woooo");
         }
       }
     }
-
-    if(open < 0) throw runtime_error("open < 0");
   }
-
-  int total_genes = genes.size();
 
   for(int pos=0; pos<n; pos++){
     int i = gen_type[pos];
     int m = marker[pos];
 
-    closed_values[pos] = total_genes - open;
-    if(gen_status[i] < 0) throw runtime_error("asd");
-    if(open < 0) throw runtime_error("open < 0");
+    closed_values[pos] = genes.size() - open_genes.size();
+    //if(gen_status[i] < 0) throw runtime_error("asd");
+    if(genes.find(i) == genes.end()) continue;
 
     if(m == S){
-      if(gen_status[i] == 0){
-        open++;
-      }
+      open_genes.insert(i);
       gen_status[i]++;
       
     } else if(m == E) {
       if(gen_status[i] > 0){
         gen_status[i]--;
         if(gen_status[i] == 0){
-          open--;
+          open_genes.erase(i);
+          if(genes.find(i) == genes.end()) throw runtime_error("woooo");
         }
       }
     }
 
-    if(gen_status[i] < 0) throw runtime_error("asd");
-    if(open < 0) throw runtime_error("open < 0");
+    //if(gen_status[i] < 0) throw runtime_error("asd");
   }
 
   int highest = 0;
@@ -90,6 +98,11 @@ int main(){
       highest_pos = pos;
     }
   }
+
+  // e1 s2 e2 s1 
+  // e1 s2 e2 s1 e1 s2 e2 s1 
+  //    |---|
+  //          |---|
 
   printf("%d %d\n", highest_pos+1, highest);
 }
