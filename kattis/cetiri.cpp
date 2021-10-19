cb89a677d2361f87d87b8cc5d3702ad7e6c102c2
Chris Vilches <felovilches@gmail.com>
2021-10-20 00:21:17 +0900

    refactor cetiri

--- kattis/cetiri.cpp
+++ kattis/cetiri.cpp
@@ -7,27 +7,15 @@ int main() {
   cin >> a >> b >> c;
 
   vector<int> nums{a, b, c};
-  unordered_map<int, int> diffs;
+  vector<int> diffs;
   sort(nums.begin(), nums.end());
 
-  for (int i = 0; i < (int)nums.size() - 1; i++) {
-    int diff = nums[i + 1] - nums[i];
-    if (diffs.find(diff) == diffs.end()) {
-      diffs[diff] = 1;
-    } else {
-      diffs[diff]++;
-    }
-  }
+  for (int i = 0; i < (int)nums.size() - 1; i++)
+    diffs.push_back(nums[i + 1] - nums[i]);
 
-  vector<int> possible_diffs;
+  sort(diffs.begin(), diffs.end());
 
-  for (auto it = diffs.begin(); it != diffs.end(); ++it) {
-    possible_diffs.push_back(it->first);
-  }
-
-  sort(possible_diffs.begin(), possible_diffs.end());
-
-  int min_diff = possible_diffs[0];
+  int min_diff = diffs[0];
 
   for (int i = 0; i < (int)nums.size() - 1; i++) {
     if (nums[i] + min_diff != nums[i + 1]) {
