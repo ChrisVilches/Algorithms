6dd813e6f72f1563a519465fa6f4ef8daa8dd657
Chris Vilches <felovilches@gmail.com>
2021-10-20 04:27:57 +0900

    uri dark roads

---
+++ urionlinejudge/1152-dark_roads.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+struct DisjointSets {
+  int *parent, *rnk;
+  int n;
+
+  DisjointSets(int n) {
+    this->n = n;
+    parent = new int[n + 1];
+    rnk = new int[n + 1];
+    for (int i = 0; i <= n; i++) {
+      rnk[i] = 0;
+      parent[i] = i;
+    }
+  }
+
+  int find(int u) {
+    if (u != parent[u]) parent[u] = find(parent[u]);
+    return parent[u];
+  }
+
+  void merge(int x, int y) {
+    x = find(x), y = find(y);
+    if (rnk[x] > rnk[y])
+      parent[y] = x;
+    else
+      parent[x] = y;
+
+    if (rnk[x] == rnk[y]) rnk[y]++;
+  }
+};
+
+struct Graph {
+  int V, E;
+  vector<pair<int, pii>> edges;
+
+  Graph(int V, int E) : V(V), E(E) {}
+
+  void add_edge(int u, int v, int w) { edges.push_back({w, {u, v}}); }
+
+  int kruskal_mst() {
+    int mst_wt = 0;
+    sort(edges.begin(), edges.end());
+
+    DisjointSets ds(V);
+
+    vector<pair<int, pii>>::iterator it;
+    for (it = edges.begin(); it != edges.end(); it++) {
+      int u = it->second.first;
+      int v = it->second.second;
+
+      int set_u = ds.find(u);
+      int set_v = ds.find(v);
+
+      if (set_u != set_v) {
+        mst_wt += it->first;
+        ds.merge(set_u, set_v);
+      }
+    }
+
+    return mst_wt;
+  }
+};
+
+int n, m;
+
+void solve() {
+  Graph g(n, m);
+  int full_graph_weight = 0;
+  for (int i = 0; i < m; i++) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    full_graph_weight += w;
+    g.add_edge(u, v, w);
+  }
+  cout << (full_graph_weight - g.kruskal_mst()) << endl;
+}
+
+int main() {
+  while (scanf("%d %d", &n, &m) == 2) {
+    if (n == 0 && m == 0) break;
+    solve();
+  }
+}
