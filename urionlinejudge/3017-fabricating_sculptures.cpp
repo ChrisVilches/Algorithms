c965bf47dbaabfdef05b062036a3cb9c440c83b5
Chris Vilches <felovilches@gmail.com>
2021-10-21 00:43:03 +0900

    fabricating sculptures refactor

--- urionlinejudge/3017-fabricating_sculptures.cpp
+++ urionlinejudge/3017-fabricating_sculptures.cpp
@@ -4,62 +4,33 @@
 using namespace std;
 typedef long long int ll;
 
-int MOD = 1000000007;
-
-ll memo[5001][5001];
-ll memo2[5001][5001];
+int MOD = 1e9 + 7;
+int memo[5001][5001];
 
 ll dp(int s, int b) {
   if (s == 0 || b == 0) return 0;
   if (s > b) return 0;
   if (s == 1 || b == 1 || b == s) return 1;
   if (memo[s][b] != -1) return memo[s][b];
-
-  ll sum;
-  sum = 0;
-  sum = (sum + dp(s, b - s)) % MOD;
-  sum = (sum + dp(s - 1, b - 1)) % MOD;
-  sum = (sum + dp(s - 1, b - 1)) % MOD;
-  sum = (sum - dp(s - 2, b - 2)) % MOD;
-  memo[s][b] = sum % MOD;
-  return memo[s][b];
+  ll result = dp(s, b - s) + 2 * dp(s - 1, b - 1) - dp(s - 2, b - 2);
+  return memo[s][b] = result % MOD;
 }
 
-ll dp2(int s, int b) {
-  if (s == 0 || b == 0) return 0;
-  if (s > b) return 0;
+int calculate(int s, int b) {
   if (s == 1 || b == 1 || b == s) return 1;
-  if(memo2[s][b] != -1) return memo2[s][b];
-
   ll sum = 0;
   for (int i = 0; i < min(s, b); i++) {
     sum += (i + 1) * dp(s - i, b - s);
     sum = sum % MOD;
   }
-
-  return memo2[s][b] = (int)(sum % MOD);
+  return (int)sum;
 }
 
 int main() {
   int S, B;
-  /*
   memset(memo, -1, sizeof memo);
- for(int i=0; i<20; i++){
-   for(int j=0; j<20; j++){
-     cout << dp(i, j) << " ";
-   }
-   cout << endl;
- }*/
- for(int i=0; i<5001; i++){
-   for(int j=0; j<5001; j++){
-     memo[i][j] = -1;
-     memo2[i][j] = -1;
-   }
- }
-  // memset(memo, -1, sizeof memo);
-  //memset(memo2, -1, sizeof memo2);
+
   while (scanf("%d %d", &S, &B) == 2) {
-    // fprintf(stderr, "S=%d B=%d\n", S, B);
-    printf("%d\n", dp2(S, B));
+    printf("%d\n", calculate(S, B));
   }
 }
