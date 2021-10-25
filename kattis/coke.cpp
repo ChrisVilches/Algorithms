e77363768a4a5339c8ab057709079467de1d3987
Chris Vilches <felovilches@gmail.com>
2021-10-25 21:25:48 +0900

    coke refactor

--- kattis/coke.cpp
+++ kattis/coke.cpp
@@ -8,26 +8,19 @@ int total_money;
 
 int cokes(int C, int n5, int n10) {
   int n1 = total_money - (n5 * 5) - (n10 * 10);
-  //if(n1 < 0 || n5 < 0)
 
+  if (n1 < 0 || n5 < 0 || n10 < 0) return INF;
   if (C == 0) return 0;
-
   if (memo[C][n5][n10] != -1) return memo[C][n5][n10];
 
-  int using1 = INF;
-  int using1_5 = INF;
-  int using5 = INF;
-  int using10 = INF;
-  int using1_10 = INF;
+  int all1 = C * 8;
+  int use1 = 8 + cokes(C - 1, n5, n10);
+  int use1_5 = 4 + cokes(C - 1, n5 - 1, n10);
+  int use5 = 2 + cokes(C - 1, n5 - 2, n10);
+  int use10 = 1 + cokes(C - 1, n5, n10 - 1);
+  int use1_10 = 4 + cokes(C - 1, n5 + 1, n10 - 1);
 
-  if (n1 >= 8) using1 = 8 + cokes(C - 1, n5, n10);
-  if (n1 >= 3 && n5 >= 1) using1_5 = 4 + cokes(C - 1, n5 - 1, n10);
-  if (n5 >= 2) using5 = 2 + cokes(C - 1, n5 - 2, n10);
-  if (n10 >= 1) using10 = 1 + cokes(C - 1, n5, n10 - 1);
-  if (n1 >= 3 && n10 >= 1) using1_10 = 4 + cokes(C - 1, n5 + 1, n10 - 1);
-
-  return memo[C][n5][n10] =
-             min({C * 8, using1, using1_5, using5, using10, using1_10});
+  return memo[C][n5][n10] = min({all1, use1, use1_5, use5, use10, use1_10});
 }
 
 void solve() {
