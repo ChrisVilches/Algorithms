af188f6802a9d6ae8598a13d7b166ec4f805f462
Chris Vilches <felovilches@gmail.com>
2021-11-12 21:17:53 +0900

    (geometry) dazzling stars

---
+++ urionlinejudge/3015-dazzling_stars.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+struct Point {
+  ll x, y;
+
+  Point() {}
+  Point(ll x, ll y) : x(x), y(y) {}
+
+  Point operator-(const Point &p) const { return Point(x - p.x, y - p.y); }
+  ll dot(const Point &p) const { return x * p.x + y * p.y; }
+  ll cross(const Point &p) const { return x * p.y - y * p.x; }
+
+  int quadrant() const {
+    if (y != 0) return y > 0 ? 0 : 1;
+    return x > 0 ? 0 : 1;
+  }
+};
+
+int N;
+Point pts[1007];
+int brightness[1007];
+
+bool cmp(const Point &p, const Point &q) {
+  int q1 = p.quadrant();
+  int q2 = q.quadrant();
+
+  if (q1 != q2) return q1 < q2;
+  return p.cross(q) > 0;
+}
+
+bool possible() {
+  vector<Point> p;
+
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      if (brightness[i] < brightness[j]) p.push_back(pts[j] - pts[i]);
+
+  if (p.empty()) return true;
+
+  sort(p.begin(), p.end(), cmp);
+
+  p.push_back(p[0]);
+
+  for (int i = 0; i < (int)p.size() - 1; i++) {
+    ll dot = p[i].dot(p[i + 1]);
+    ll cross = p[i].cross(p[i + 1]);
+    if ((dot <= 0 || cross != 0) && cross <= 0) return true;
+  }
+
+  return false;
+}
+
+int main() {
+  while (scanf("%d", &N) == 1) {
+    for (int i = 0; i < N; i++) cin >> pts[i].x >> pts[i].y >> brightness[i];
+    cout << (possible() ? 'Y' : 'N') << endl;
+  }
+}
