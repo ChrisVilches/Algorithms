72466df9dad1ece1cb5d5c91e6c9c38eb34bc7bd
Chris Vilches <felovilches@gmail.com>
2021-11-20 20:23:51 +0900

    refactor cornering at poles

--- codeforces/cornering_at_poles.cpp
+++ codeforces/cornering_at_poles.cpp
@@ -1,8 +1,8 @@
+#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
-
 using namespace std;
 typedef long double ld;
-#define PI 3.14159265358979323846
+const ld EPS = 1e-12;
 
 inline int sgn(int n) { return n < 0 ? -1 : n > 0; }
 
@@ -39,7 +39,7 @@ struct Segment {
     return sgn1 == 0 || sgn2 == 0 || sgn1 == sgn2;
   }
 
-  ld dist_point(Point P) {
+  ld dist(Point P) {
     Segment L = *this;
     if ((P - L.p) * L.to_vec() < 0)
       return P.dist(L.p);
@@ -50,43 +50,37 @@ struct Segment {
   }
 };
 
-struct Circle;
-array<Segment, 2> point_circle_tangents(Point p, Circle c);
-
 struct Circle {
   Point center;
   ld radius;
   Circle(Point c, ld r) : center(c), radius(r) {}
 
-  inline bool intersect(Segment s) { return s.dist_point(center) < radius; }
+  inline bool intersect(Segment s) { return s.dist(center) + EPS < radius; }
 
   ld arc(Segment incoming_tan, Segment outgoing_tan) {
     Point a = center.to(incoming_tan.q);
     Point b = center.to(outgoing_tan.p);
     ld angle = acos(a * b / (a.magnitude() * b.magnitude()));
-    if (tangents_reflex_angle(incoming_tan, outgoing_tan)) angle -= 2 * PI;
+    if (tangents_reflex_angle(incoming_tan, outgoing_tan)) angle -= 2 * M_PI;
     return fabs(angle * radius);
   }
 
   bool arc_intersects(Circle c, Segment incoming_tan, Segment outgoing_tan) {
-    if (center.dist(c.center) >= radius + c.radius) return false;
-    Segment tan = incoming_tan;
+    if (center.dist(c.center) > radius + c.radius) return false;
 
     if (tangents_reflex_angle(incoming_tan, outgoing_tan)) {
+      Segment tan = incoming_tan;
       bool intersect1 = sgn(tan.to_vec() ^ tan.q.to(center)) ==
                         sgn(center.to(c.center) ^ center.to(outgoing_tan.p));
       tan = outgoing_tan.invert();
       bool intersect2 = sgn(tan.to_vec() ^ tan.q.to(center)) ==
                         sgn(center.to(c.center) ^ center.to(incoming_tan.q));
       return intersect1 || intersect2;
-    } else {
-      bool intersect1 = sgn(tan.to_vec() ^ tan.q.to(center)) !=
-                        sgn(tan.q.to(center) ^ center.to(c.center));
-      tan = outgoing_tan.invert();
-      bool intersect2 = sgn(tan.to_vec() ^ tan.q.to(center)) !=
-                        sgn(tan.q.to(center) ^ center.to(c.center));
-      return intersect1 && intersect2;
     }
+
+    Segment to_circle = Segment(center, c.center);
+    return !incoming_tan.same_direction(to_circle) &&
+           !outgoing_tan.invert().same_direction(to_circle);
   }
 
   array<Segment, 4> tangents_to(Circle c) {
@@ -94,10 +88,19 @@ struct Circle {
                              internal_tangents(c)[0], internal_tangents(c)[1]};
   }
 
+  array<Segment, 2> tangents_from_point(Point p) {
+    ld a = radius, b = p.dist(center);
+    ld th = acos(a / b);
+    ld d = atan2(p.y - center.y, p.x - center.x);
+    ld d1 = d + th, d2 = d - th;
+    return array<Segment, 2>{
+        Segment(p, Point(center.x + a * cos(d1), center.y + a * sin(d1))),
+        Segment(p, Point(center.x + a * cos(d2), center.y + a * sin(d2)))};
+  }
+
  private:
   bool tangents_reflex_angle(Segment in, Segment out) {
-    return sgn(in.to_vec() ^ in.q.to(center)) ==
-           sgn(in.q.to(center) ^ center.to(out.p));
+    return in.same_direction(Segment(center, out.p));
   }
 
   array<Segment, 2> external_tangents(Circle c) {
@@ -109,8 +112,8 @@ struct Circle {
 
   array<Segment, 2> internal_tangents(Circle c) {
     Point mid = Segment(center, c.center).scale(0.5L).q;
-    auto point_tans1 = point_circle_tangents(mid, c);
-    auto point_tans2 = point_circle_tangents(mid, *this);
+    auto point_tans1 = c.tangents_from_point(mid);
+    auto point_tans2 = tangents_from_point(mid);
     return array<Segment, 2>{Segment(point_tans2[0].q, point_tans1[0].q),
                              Segment(point_tans2[1].q, point_tans1[1].q)};
   }
@@ -120,74 +123,55 @@ vector<Circle> poles;
 Point robot(0, 0), goal;
 ld min_dist;
 
-array<Segment, 2> point_circle_tangents(Point p, Circle c) {
-  ld a = c.radius, b = p.dist(c.center);
-  ld th = acos(a / b);
-  ld d = atan2(p.y - c.center.y, p.x - c.center.x);
-  ld d1 = d + th, d2 = d - th;
-  return array<Segment, 2>{
-      Segment(p, Point(c.center.x + a * cos(d1), c.center.y + a * sin(d1))),
-      Segment(p, Point(c.center.x + a * cos(d2), c.center.y + a * sin(d2)))};
-}
-
-bool movement_possible(Segment& s, int ignore_pole_idx1, int ignore_pole_idx2) {
-  for (int i = 0; i < (int)poles.size(); i++) {
-    if (i == ignore_pole_idx1 || i == ignore_pole_idx2) continue;
+bool movement_valid(Segment& s) {
+  for (int i = 0; i < (int)poles.size(); i++)
     if (poles[i].intersect(s)) return false;
-  }
   return true;
 }
 
-bool valid_tangent_traversal(int pole_idx, Segment& in_tan, Segment& out_tan) {
+bool tangent_movement_valid(int pole_idx, Segment& in_tan, Segment& out_tan) {
   if (!in_tan.same_direction(out_tan)) return false;
   for (int i = 0; i < (int)poles.size(); i++) {
     if (pole_idx == i) continue;
     if (poles[pole_idx].arc_intersects(poles[i], in_tan, out_tan)) return false;
   }
-  return true;
+  return movement_valid(out_tan);
 }
 
 void traverse(int pole_idx, Segment& from, bitset<8> visited, ld dist) {
   if (dist > min_dist) return;
   Circle& curr_pole = poles[pole_idx];
 
-  for (Segment& tangent : point_circle_tangents(goal, curr_pole)) {
+  for (Segment& tangent : curr_pole.tangents_from_point(goal)) {
     tangent = tangent.invert();
-    if (!valid_tangent_traversal(pole_idx, from, tangent)) continue;
-    if (!movement_possible(tangent, pole_idx, -1)) continue;
+    if (!tangent_movement_valid(pole_idx, from, tangent)) continue;
     min_dist =
         min(min_dist, dist + tangent.length() + curr_pole.arc(from, tangent));
     return;
   }
 
-  for (int i = 0; i < (int)poles.size(); i++) {
-    if (visited[i]) continue;
-
-    for (Segment& tangent : curr_pole.tangents_to(poles[i])) {
-      if (!valid_tangent_traversal(pole_idx, from, tangent)) continue;
-      if (!movement_possible(tangent, pole_idx, i)) continue;
-
-      ld arc = curr_pole.arc(from, tangent);
-      bitset<8> v = visited;
-      traverse(i, tangent, v.set(i), dist + tangent.length() + arc);
-    }
-  }
+  for (int i = 0; i < (int)poles.size(); i++)
+    if (!visited[i])
+      for (Segment& tangent : curr_pole.tangents_to(poles[i]))
+        if (tangent_movement_valid(pole_idx, from, tangent)) {
+          ld arc = curr_pole.arc(from, tangent);
+          bitset<8> v = visited;
+          traverse(i, tangent, v.set(i), dist + tangent.length() + arc);
+        }
 }
 
 void solve() {
   Segment direct_robot_goal(robot, goal);
 
-  if (movement_possible(direct_robot_goal, -1, -1)) {
+  if (movement_valid(direct_robot_goal)) {
     printf("%.5Lf\n", direct_robot_goal.length());
     return;
   }
 
   for (int i = 0; i < (int)poles.size(); i++)
-    for (Segment& tangent : point_circle_tangents(robot, poles[i]))
-      if (movement_possible(tangent, i, -1)) {
-        bitset<8> visited;
-        traverse(i, tangent, visited.set(i), tangent.length());
-      }
+    for (Segment& tangent : poles[i].tangents_from_point(robot))
+      if (movement_valid(tangent))
+        traverse(i, tangent, bitset<8>(1 << i), tangent.length());
 
   printf("%.8Lf\n", min_dist == DBL_MAX ? 0.0 : min_dist);
 }
