a23bf7d6ac6039a78868db6f7cde7699667e6a3d
Chris Vilches <felovilches@gmail.com>
2021-11-24 07:21:08 +0900

    refactor ceiling function (kattis)

--- kattis/ceiling.cpp
+++ kattis/ceiling.cpp
@@ -4,70 +4,68 @@ using namespace std;
 
 int n, k;
 
-// no destructor
 struct Tree {
   int value = -1;
   Tree* left = nullptr;
   Tree* right = nullptr;
+  ~Tree() {
+    if (left) delete (left);
+    if (right) delete (right);
+  }
+
+  void insert(int node) {
+    if (value <= node) {
+      if (right) {
+        right->insert(node);
+      } else {
+        right = new Tree();
+        right->value = node;
+      }
+    } else {
+      if (left) {
+        left->insert(node);
+      } else {
+        left = new Tree();
+        left->value = node;
+      }
+    }
+  }
+
+  long long hash(int factor) {
+    long long total = 0;
+
+    if (left)
+      total += 0x7c2123c * left->hash(factor + 1) * factor;
+    else
+      total += 0xc343455 * factor;
+
+    if (right)
+      total += 0x919113 * right->hash(factor + 2) * factor;
+    else
+      total += 0x2af4343 * factor;
+
+    return total;
+  }
 };
 
-void insert(Tree* t, int node){
-  if(t->value <= node){
-    if(t->right){
-      insert(t->right, node);
-    } else {
-      t->right = new Tree();
-      t->right->value = node;
-    }
-  } else {
-    if(t->left){
-      insert(t->left, node);
-    } else {
-      t->left = new Tree();
-      t->left->value = node;
-    }
-  }
-}
-
-long long hash_tree(Tree* t, int factor){
-  long long total = 0;
-
-  if(t->left)
-    total += 0x7c2123c * hash_tree(t->left, factor + 1) * factor;
-  else
-    total += 0xc343455 * factor;
-
-  if(t->right)
-    total += 0x919113 * hash_tree(t->right, factor + 2) * factor;
-  else
-    total += 0x2af4343 * factor;
-
-  return total;
-}
-
-void solve(){
+void solve() {
   int node;
-  unordered_map<long long, int> counts;
-  for(int i=0; i<n; i++){
+  unordered_set<long long> trees;
+  for (int i = 0; i < n; i++) {
     Tree t;
     cin >> t.value;
-    for(int j=1; j<k; j++){
+    for (int j = 1; j < k; j++) {
       cin >> node;
-      insert(&t, node);
+      t.insert(node);
     }
 
-    long long h = hash_tree(&t, 1);
-
-    if(counts.find(h) != counts.end())
-      counts[h]++;
-    else
-      counts[h] = 1;
+    trees.emplace(t.hash(1));
   }
 
-  cout << counts.size() << endl;
+  cout << trees.size() << endl;
 }
 
-int main(){
-  while(scanf("%d %d", &n, &k) == 2) solve();
+int main() {
+  while (scanf("%d %d", &n, &k) == 2) solve();
   return 0;
 }
