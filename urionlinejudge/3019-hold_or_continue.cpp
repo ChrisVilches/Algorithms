4d664fe63e7807727039341ea57e079eeccd2728
Chris Vilches <felovilches@gmail.com>
2021-11-28 18:22:59 +0900

    refactor Hold or Continue

--- urionlinejudge/3019-hold_or_continue.cpp
+++ urionlinejudge/3019-hold_or_continue.cpp
@@ -2,31 +2,34 @@
 using namespace std;
 typedef long double ld;
 
-const int N = 75;
-ld a[N + 3];
-ld dp[N + 3][N + 3];
-int ans[N + 3][N + 3][N + 3];
+#define GOAL_SCORE 75
+
+ld solve_memo[GOAL_SCORE][GOAL_SCORE];
+ld p_memo[GOAL_SCORE][GOAL_SCORE][GOAL_SCORE];
 
 ld solve(int A, int B) {
+  if (solve_memo[A][B] > -1) return solve_memo[A][B];
   ld p = 0.5L;
+
+  array<ld, GOAL_SCORE + 1> a;
 
   for (int t = 0; t < 100; t++) {
     swap(A, B);
 
-    for (int i = 0; i <= N; i++) {
-      if (i == 0 || A + i > N) {
+    for (int i = 0; i <= GOAL_SCORE; i++) {
+      if (i == 0 || A + i > GOAL_SCORE) {
         a[i] = 1 - p;
-      } else if (A + i == N) {
+      } else if (A + i == GOAL_SCORE) {
         a[i] = 1;
       } else {
-        a[i] = 1 - dp[B][A + i];
+        a[i] = 1 - solve(B, A + i);
       }
     }
 
-    for (int i = N - A - 1; i >= 0; i--) {
+    for (int i = GOAL_SCORE - A - 1; i >= 0; i--) {
       ld curr = 1 - p;
       for (int j = 2; j <= 6; j++) {
-        if (i + j <= N) {
+        if (i + j <= GOAL_SCORE) {
           curr += a[i + j];
         } else {
           curr += 1 - p;
@@ -37,77 +40,43 @@ ld solve(int A, int B) {
     p = a[0];
   }
 
-  return p;
+  return solve_memo[A][B] = p;
 }
 
-void calculate_answer(int A, int B) {
-  for (int i = 0; i <= N; i++) {
-    if (A + i > N) {
-      a[i] = 1 - dp[B][A];
-    } else if (A + i == N) {
-      a[i] = 1;
-    } else {
-      a[i] = 1 - dp[B][A + i];
-    }
-  }
-  for (int i = N - A; i >= 0; i--) {
-    ld curr = 1 - dp[B][A];
-    for (int j = 2; j <= 6; j++) {
-      if (i + j <= N) {
-        curr += a[i + j];
-      } else {
-        curr += 1 - dp[B][A];
-      }
-    }
+ld p(int i, int j, int k);
 
-    if (curr / 6L > a[i]) {
-      a[i] = curr / 6;
-      ans[A][B][i] = 0;
-    } else {
-      ans[A][B][i] = 1;
-    }
-  }
+ld p_hold(int i, int j, int k) { return 1 - p(j, i + k, 0); }
+
+ld p_continue(int i, int j, int k) {
+  ld result = 1 - p(j, i, 0);
+  for (int dice = 2; dice <= 6; dice++) result += p(i, j, k + dice);
+  return result / 6;
+}
+
+ld p(int i, int j, int k) {
+  if (j == 75) return 0;
+  if (i + k > 75) return 1 - p(j, i, 0);
+  if (k == 0) return solve(i, j);
+
+  if (p_memo[i][j][k] > -1) return p_memo[i][j][k];
+
+  return p_memo[i][j][k] = max(p_hold(i, j, k), p_continue(i, j, k));
 }
 
 int main() {
-  for (int i = 0; i < N; i++) {
-    dp[N][i] = 1;
-  }
+  memset(p_memo, -1, sizeof p_memo);
+  memset(solve_memo, -1, sizeof solve_memo);
 
-  for (int sum = 2 * N; sum >= 0; sum--) {
-    for (int A = 0; A < N; A++) {
-      int B = sum - A;
-      if (B < 0 || B >= N) continue;
-      if (A == N - 1) {
-        dp[A][B] = 0;
-        continue;
-      }
-      if (B == N - 1) {
-        dp[A][B] = 1;
-        continue;
-      }
-      dp[A][B] = solve(A, B);
-    }
-
-    for (int A = 0; A < N - 1; A++) {
-      int B = sum - A;
-      if (B < 0 || B >= N - 1) continue;
-      calculate_answer(A, B);
-    }
-  }
-  /*
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 5; j++) {
-        fprintf(stderr, "dp[%d][%d] = %.5f\n", i, j, (double)dp[i][j]);
-      }
-    }
-  */
   int q;
   while (scanf("%d", &q) == 1)
     while (q--) {
       int A, B, X;
       cin >> A >> B >> X;
-      if (ans[A][B][X]) {
+
+      ld ph = p_hold(A, B, X);
+      ld pc = p_continue(A, B, X);
+
+      if (ph > pc) {
         cout << 'H' << endl;
       } else {
         cout << 'C' << endl;
