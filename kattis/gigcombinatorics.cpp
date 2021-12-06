c5bc29e7388f39d261a1e2f0325dd27fae9aa733
Chris Vilches <felovilches@gmail.com>
2021-12-07 07:45:17 +0900

    kattis - gig combinatorics (refactor)

--- kattis/gigcombinatorics.cpp
+++ kattis/gigcombinatorics.cpp
@@ -2,14 +2,15 @@
 
 using namespace std;
 typedef long long ll;
-#define MAX 1000010
+#define MAX 1000001
 
-int N, nums[MAX];
+int N;
+char nums[MAX];
 ll memo[2][MAX];
 const ll MOD = 1e9 + 7;
 
-inline ll mult(ll a, ll b) { return (a * b) % MOD; }
-inline ll add(ll a, ll b) { return (a + b) % MOD; }
+inline void mult(ll& a, ll b) { a = (a * b) % MOD; }
+inline void add(ll& a, ll b) { a = (a + b) % MOD; }
 
 ll dp(bool first, int n) {
   if (n == N) return 0;
@@ -17,10 +18,10 @@ ll dp(bool first, int n) {
 
   ll total = dp(false, n + 1);
 
-  if (nums[n] == 2 && !first)
-    total = mult(total, 2);
-  else if (nums[n] == 3)
-    total = add(total, 1);
+  if (nums[n] == '2' && !first)
+    mult(total, 2);
+  else if (nums[n] == '3')
+    add(total, 1);
 
   return memo[first][n] = total;
 }
@@ -28,15 +29,20 @@ ll dp(bool first, int n) {
 int main() {
   while (scanf("%d", &N) == 1) {
     memset(memo, -1, sizeof memo);
-    for (int i = 0; i < N; i++) scanf("%d", &nums[i]);
+    for (int i = 0; i < N; i++) {
+      char c = '\0';
+      // Read '1', '2', '3' while skipping spaces.
+      while (!(c & 16)) c = getchar_unlocked();
+      nums[i] = c;
+    }
 
     ll total = 0;
     ll ones = 0;
     for (int i = 0; i < N; i++)
-      if (nums[i] == 1)
+      if (nums[i] == '1')
         ones++;
-      else if (nums[i] == 2)
-        total = add(total, mult(ones, dp(true, i)));
+      else if (nums[i] == '2')
+        add(total, ones * dp(true, i));
 
     printf("%lld\n", total);
   }
