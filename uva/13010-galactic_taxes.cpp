9675b94a6ae8c0e9898d50a14049c13750ec323a
Chris Vilches <felovilches@gmail.com>
2021-12-08 20:13:16 +0900

    refactor galactic taxes

--- uva/13010-galactic_taxes.cpp
+++ uva/13010-galactic_taxes.cpp
@@ -1,75 +1,66 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<double, double> pdd;
+typedef pair<int, int> pii;
 typedef pair<double, int> pdi;
 int N, M;
+double dist[1001];
 
-double dijkstra(double t, vector<vector<pair<int, pdd>>> &adj){
-  double dist[N];
-  fill(dist, dist+N, 10e9);
+vector<vector<pair<int, pii>>> graph;
+
+double dijkstra(double t) {
+  fill(dist, dist + N, 1e9);
   priority_queue<pdi, vector<pdi>, greater<pdi>> q;
 
   q.push(make_pair(0, 0));
   dist[0] = 0;
 
-  while(!q.empty()){
+  while (!q.empty()) {
     pair<double, int> u = q.top();
     q.pop();
-    vector<pair<int, pdd>> neighbors = adj[u.second];
-    for(pair<int, pdd> neighbor : neighbors){
+    vector<pair<int, pii>> neighbors = graph[u.second];
+    for (pair<int, pii> neighbor : neighbors) {
       int v = neighbor.first;
-      double alt = dist[u.second] + (neighbor.second.first * t + neighbor.second.second);
-      if(alt < dist[v]){
+      double alt =
+          dist[u.second] + (neighbor.second.first * t + neighbor.second.second);
+      if (alt < dist[v]) {
         dist[v] = alt;
         q.push(make_pair(alt, v));
       }
     }
   }
 
-  return dist[adj.size() - 1];
+  return dist[N - 1];
 }
 
-void solve(){
-  vector<vector<pair<int, pdd>>> adj;
+int main() {
+  scanf("%d %d", &N, &M);
+  graph.assign(N, vector<pair<int, pii>>());
 
-  for(int i=0; i<N; i++){
-    vector<pair<int, pdd>> list;
-    adj.push_back(list);
-  }
-
-  for(int i=0; i<M; i++){
-    int source, target;
-    double a, b;
-    cin >> source >> target >> a >> b;
+  while (M--) {
+    int source, target, a, b;
+    scanf("%d %d %d %d", &source, &target, &a, &b);
     source--;
     target--;
 
-    adj[source].push_back(make_pair(target, make_pair(a, b)));
-    adj[target].push_back(make_pair(source, make_pair(a, b)));
+    graph[source].push_back({target, {a, b}});
+    graph[target].push_back({source, {a, b}});
   }
+
   double left = 0;
-  double right = 60*24;
+  double right = 60 * 24;
   int iters = 80;
 
-  while(iters--){
-    double third = (right-left)/3;
-    double third1 = left + third;
-    double third2 = right - third;
-    double d1 = dijkstra(third1, adj);
-    double d2 = dijkstra(third2, adj);
+  while (iters--) {
+    double third = (right - left) / 3;
+    double t1 = left + third;
+    double t2 = right - third;
 
-    if(d1 < d2)
-      left = third1;
+    if (dijkstra(t1) < dijkstra(t2))
+      left = t1;
     else
-      right = third2;
+      right = t2;
   }
 
-  printf("%0.5f\n", dijkstra(left, adj));
-}
-
-int main(){
-  while(scanf("%d %d", &N, &M) == 2)
-    solve();
-  return 0;
+  printf("%0.5f\n", dijkstra(left));
 }
