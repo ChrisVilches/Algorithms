6586a38b1a8e075f2534afa93c60cbea11ef8ea5
Chris Vilches <felovilches@gmail.com>
2021-12-10 07:33:39 +0900

    horrible queries almost 100% refactored

--- spoj/HORRIBLE-horrible_queries_BIT.cpp
+++ spoj/HORRIBLE-horrible_queries_BIT.cpp
@@ -7,16 +7,14 @@ struct BIT {
   BIT() {}
   BIT(int n) : bit_n(n + 1) { A.assign(bit_n, 0); }
 
-  ll query(int i) {
-    i++;
-    ll sum = 0;
-    for (; i > 0; i -= i & -i) sum += A[i];
-    return sum;
+  void update(int i, ll val) {
+    for (; i < bit_n; i += i & -i) A[i] += val;
   }
 
-  void update(int i, ll v) {
-    i++;
-    for (; i <= bit_n; i += i & -i) A[i] += v;
+  ll query(int i) {
+    ll ret = 0;
+    for (; i > 0; i -= i & -i) ret += A[i];
+    return ret;
   }
 
  private:
@@ -26,24 +24,20 @@ struct BIT {
 
 struct RangeBIT {
   RangeBIT(int n) {
-    bit1 = BIT(n + 2);
-    bit2 = BIT(n + 2);
+    bit1 = BIT(n);
+    bit2 = BIT(n);
   }
 
-  void update_range(int i, int j, ll v) {
-    i++;
-    j++;
-    bit1.update(i, v);
-    bit1.update(j + 1, -v);
-    bit2.update(i, v * (i - 1));
-    bit2.update(j + 1, -v * j);
+  void update_range(int l, int r, ll v) {
+    bit1.update(l, v);
+    bit1.update(r + 1, -v);
+    bit2.update(l, -v * (l - 1));
+    bit2.update(r + 1, v * r);
   }
 
-  ll range_sum(int i, int j) {
-    i++;
-    j++;
-    ll a = (bit1.query(j) * j) - bit2.query(j);
-    ll b = (bit1.query(i - 1) * (i - 1)) - bit2.query(i - 1);
+  ll range_sum(int l, int r) {
+    ll a = bit1.query(r) * r + bit2.query(r);
+    ll b = bit1.query(l - 1) * (l - 1) + bit2.query(l - 1);
     return a - b;
   }
 
@@ -61,9 +55,11 @@ void solve() {
     int is_range_sum_query, p, q;
     ll v;
     scanf("%d %d %d", &is_range_sum_query, &p, &q);
-    p--;
-    q--;
 
+    if (p <= 0) p = 1;  // for uDebug test case.
+
+    // q--;
+    // p--;
     if (is_range_sum_query)
       printf("%lld\n", range_bit.range_sum(p, q));
     else {
