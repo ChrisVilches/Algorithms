986d4d69a1e144810bb5b2c441053c60d35f203c
Chris Vilches <felovilches@gmail.com>
2021-12-15 22:47:46 +0900

    refactor & optimize - man elephant and mouse

--- urionlinejudge/1477-man_elephant_and_mouse.cpp
+++ urionlinejudge/1477-man_elephant_and_mouse.cpp
@@ -7,8 +7,8 @@ Micro-optimized.
 */
 
 int N, Q;
-tiii tree[4 * 100001];
-short lazy[4 * 100001];
+tiii tree[4 * 100004];
+short lazy[4 * 100004];
 
 void print(uint32_t n) {
   if (n / 10) print(n / 10);
@@ -17,14 +17,13 @@ void print(uint32_t n) {
 
 uint32_t read_int() {
   char r;
-  bool start = false;
+  bool b, start = false;
   uint32_t ret = 0;
-  start = false;
-  ret = 0;
   while (true) {
     r = getchar_unlocked();
-    if (!start && (r < 47 || r > 58)) continue;
-    if (start && (r < 47 || r > 58)) break;
+    b = (r < 47 || r > 58);
+    if (!start && b) continue;
+    if (start && b) break;
     if (start) ret *= 10;
     start = true;
     ret += r - '0';
@@ -37,32 +36,26 @@ inline tiii add_tuple(const tiii& t1, const tiii& t2) {
           get<2>(t1) + get<2>(t2)};
 }
 
-inline tiii shift_tuple(const tiii& t, const int times) {
-  auto [a, b, c] = t;
-  switch (times) {
+inline tiii shift_tuple(const tiii& t, int times) {
+  switch (times % 3) {
     case 1:
-      return {c, a, b};
+      return {get<2>(t), get<0>(t), get<1>(t)};
     case 2:
-      return {b, c, a};
+      return {get<1>(t), get<2>(t), get<0>(t)};
     default:
       return t;
   }
 }
 
-inline short add_mod3(short a, short b) {
-  if (a + b < 3) return a + b;
-  return a + b - 3;
-}
-
 inline int left(int p) { return p << 1; }
-inline int right(int p) { return (p << 1) + 1; }
+inline int right(int p) { return (p << 1) | 1; }
 
 void initialize_tree(int p, int a, int b) {
   if (a == b)
     tree[p] = {1, 0, 0};
   else {
-    initialize_tree(left(p), a, (a + b) / 2);
-    initialize_tree(right(p), (a + b) / 2 + 1, b);
+    initialize_tree(left(p), a, (a + b) >> 1);
+    initialize_tree(right(p), 1 + ((a + b) >> 1), b);
     tree[p] = add_tuple(tree[left(p)], tree[right(p)]);
   }
 }
@@ -72,8 +65,8 @@ void propagate_one_level(int node, int a, int b) {
   tree[node] = shift_tuple(tree[node], lazy[node]);
 
   if (a != b) {
-    lazy[left(node)] = add_mod3(lazy[left(node)], lazy[node]);
-    lazy[right(node)] = add_mod3(lazy[right(node)], lazy[node]);
+    lazy[left(node)] += lazy[node];
+    lazy[right(node)] += lazy[node];
   }
 
   lazy[node] = 0;
@@ -87,14 +80,14 @@ void change_symbol(int node, int a, int b, int i, int j) {
     tree[node] = shift_tuple(tree[node], 1);
 
     if (a != b) {
-      lazy[left(node)] = add_mod3(lazy[left(node)], 1);
-      lazy[right(node)] = add_mod3(lazy[right(node)], 1);
+      lazy[left(node)]++;
+      lazy[right(node)]++;
     }
     return;
   }
 
-  change_symbol(left(node), a, (a + b) / 2, i, j);
-  change_symbol(right(node), (a + b) / 2 + 1, b, i, j);
+  change_symbol(left(node), a, (a + b) >> 1, i, j);
+  change_symbol(right(node), 1 + ((a + b) >> 1), b, i, j);
   tree[node] = add_tuple(tree[left(node)], tree[right(node)]);
 }
 
@@ -103,33 +96,37 @@ tiii count_frequencies(int node, int a, int b, int i, int j) {
   propagate_one_level(node, a, b);
   if (i <= a && b <= j) return tree[node];
 
-  return add_tuple(count_frequencies(left(node), a, (a + b) / 2, i, j),
-                   count_frequencies(right(node), 1 + (a + b) / 2, b, i, j));
+  return add_tuple(count_frequencies(left(node), a, (a + b) >> 1, i, j),
+                   count_frequencies(right(node), 1 + ((a + b) >> 1), b, i, j));
 }
 
 int main() {
-  while (scanf("%d %d", &N, &Q) == 2) {
-    memset(lazy, 0, sizeof(short) * 4 * N);
-    initialize_tree(1, 0, N - 1);
+  char query_type;
+  int i, j;
+  tiii res;
+
+  while (scanf("%d", &N) == 1) {
+    Q = read_int();
+    memset(lazy, 0, sizeof(short) * 4 * (N + 1));
+    initialize_tree(1, 0, N);
 
     while (Q--) {
-      char query_type = '\0';
-      int i, j;
+      query_type = '\0';
       while (!(query_type & 64)) query_type = getchar_unlocked();
-      i = read_int() - 1;
-      j = read_int() - 1;
+      i = read_int();
+      j = read_int();
 
       switch (query_type) {
         case 'M':
-          change_symbol(1, 0, N - 1, i, j);
+          change_symbol(1, 0, N, i, j);
           break;
         default:
-          auto [a, b, c] = count_frequencies(1, 0, N - 1, i, j);
-          print(a);
+          res = count_frequencies(1, 0, N, i, j);
+          print(get<0>(res));
           putchar_unlocked(' ');
-          print(b);
+          print(get<1>(res));
           putchar_unlocked(' ');
-          print(c);
+          print(get<2>(res));
           putchar_unlocked('\n');
       }
     }
