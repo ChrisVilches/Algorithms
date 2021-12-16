068b397392472aa16d0f4519d08ba234e57b7766
Felipe Vilches <felovilches@gmail.com>
2021-12-16 22:40:29 +0900

    refactor generations so that segment tree is outside a struct (to get more used to coding it manually)

--- urionlinejudge/1969-generations.cpp
+++ urionlinejudge/1969-generations.cpp
@@ -1,54 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define MAX_N 100'007
 
 const ll mod = 1e9 + 7;
-ll nums[100'007];
+ll nums[MAX_N];
+ll tree[4 * MAX_N];
 
-struct Segtree {
-  Segtree(ll* arr, int n) {
-    tree.assign(4 * n, 1);
-    build(arr, 1, 0, n - 1);
+inline ll mult(ll a, ll b) { return (a * b) % mod; }
+
+ll query(int p, int L, int R, int i, int j) {
+  if (i > R || j < L) return 1;
+  if (i <= L && R <= j) return tree[p];
+
+  int m = (L + R) / 2;
+  return mult(query(2 * p, L, m, i, j), query(2 * p + 1, m + 1, R, i, j));
+}
+
+void update(int p, int L, int R, int pos, ll new_val) {
+  if (L == R) {
+    tree[p] = new_val;
+  } else {
+    int m = (L + R) / 2;
+    if (pos <= m)
+      update(2 * p, L, m, pos, new_val);
+    else
+      update(2 * p + 1, m + 1, R, pos, new_val);
+
+    tree[p] = mult(tree[2 * p], tree[2 * p + 1]);
   }
+}
 
-  ll query(int p, int L, int R, int i, int j) {
-    if (i > R || j < L) return 1;
-    if (i <= L && R <= j) return tree[p];
-
-    return mult(query(left(p), L, (L + R) / 2, i, j),
-                query(right(p), (L + R) / 2 + 1, R, i, j));
+void build_tree(int p, int L, int R) {
+  if (L == R)
+    tree[p] = nums[L];
+  else {
+    build_tree(2 * p, L, (L + R) / 2);
+    build_tree(2 * p + 1, (L + R) / 2 + 1, R);
+    tree[p] = mult(tree[2 * p], tree[2 * p + 1]);
   }
-
-  void update(int p, int L, int R, int pos, ll new_val) {
-    if (L == R) {
-      tree[p] = new_val;
-    } else {
-      int m = (L + R) / 2;
-      if (pos <= m)
-        update(left(p), L, m, pos, new_val);
-      else
-        update(right(p), m + 1, R, pos, new_val);
-
-      tree[p] = mult(tree[left(p)], tree[right(p)]);
-    }
-  }
-
- private:
-  vector<ll> tree;
-  inline int left(int p) { return p << 1; }
-  inline int right(int p) { return (p << 1) + 1; }
-  inline ll mult(int a, int b) { return ((ll)a * (ll)b) % mod; }
-
-  void build(ll* A, int p, int L, int R) {
-    if (L == R)
-      tree[p] = A[L];
-    else {
-      build(A, left(p), L, (L + R) / 2);
-      build(A, right(p), (L + R) / 2 + 1, R);
-      tree[p] = mult(tree[left(p)], tree[right(p)]);
-    }
-  }
-};
+}
 
 struct FastFibonacci {
   ll get(ll n, ll mod) {
@@ -89,7 +80,7 @@ int main() {
     nums[i] = possibilities(nums[i]);
   }
 
-  Segtree st(nums, G);
+  build_tree(1, 0, G - 1);
 
   while (E--) {
     char type;
@@ -100,10 +91,10 @@ int main() {
     bool is_update = type == '-';
 
     if (is_update)
-      st.update(1, 0, G - 1, (int)a - 1, possibilities(b));
+      update(1, 0, G - 1, (int)a - 1, possibilities(b));
     else
-      printf("%lld\n", st.query(1, 0, G - 1, (int)a - 1, (int)b - 1));
+      printf("%lld\n", query(1, 0, G - 1, (int)a - 1, (int)b - 1));
   }
 
-  printf("%lld\n", st.query(1, 0, G - 1, 0, G - 1));
+  printf("%lld\n", query(1, 0, G - 1, 0, G - 1));
 }
