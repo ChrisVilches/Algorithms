17c81a7e7fe635fc193a71e6c06e020ba686494d
Chris Vilches <felovilches@gmail.com>
2021-12-21 01:38:46 +0900

    uva - zeros and ones

---
+++ uva/10324-zeros_and_ones.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+char sequence[1'000'007];
+ll psum[1'000'007];
+
+ll sum_range(int l, int r) { return psum[r + 1] - psum[l]; }
+
+int main() {
+  int test = 1;
+
+  while (scanf("%s", sequence) == 1) {
+    int N = strlen(sequence);
+    if (N == 0) break;
+
+    psum[0] = 0;
+    for (int i = 0; i < N; i++) {
+      int value = sequence[i] - '0';
+      psum[i + 1] = psum[i] + value;
+    }
+
+    int Q;
+    scanf("%d", &Q);
+
+    printf("Case %d:\n", test++);
+
+    while (Q--) {
+      int i, j;
+      scanf("%d %d", &i, &j);
+      if (i > j) swap(i, j);
+
+      int ones_amount = j - i + 1;
+      bool all_same = sum_range(i, j) == ones_amount || sum_range(i, j) == 0;
+
+      printf("%s\n", all_same ? "Yes" : "No");
+    }
+  }
+}
