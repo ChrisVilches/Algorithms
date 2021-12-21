d8e79e1bc6707a0b9fdbcf16009fa46b6d2ec522
Chris Vilches <felovilches@gmail.com>
2021-12-21 19:12:55 +0900

    refactor dijkstra shortestpath1

--- kattis/shortestpath1.cpp
+++ kattis/shortestpath1.cpp
@@ -8,50 +8,45 @@ const int INF = 10e8;
 
 int nodes, edges, init_node, queries;
 int dist[10000];
-int previous[10000];
-auto comp = []( pair<int, int> a, pair<int, int> b ) { return a.second > b.second; };
+auto comp = [](pair<int, int> a, pair<int, int> b) { return a.second > b.second; };
 
-void dijkstra(map<int, vector<pair<int, int>>> &graph){
+void dijkstra(map<int, vector<pair<int, int>>>& graph) {
   fill(dist, dist + nodes, INF);
-  fill(previous, previous + nodes, -1);
 
   priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> q(comp);
 
-  q.push(make_pair(init_node, 0));
+  q.push({0, init_node});
   dist[init_node] = 0;
 
-  while(!q.empty()){
-    pair<int, int> u = q.top();
+  while (!q.empty()) {
+    auto [_, u] = q.top();
     q.pop();
-    auto neighbors = graph[u.first];
-    for(pair<int, int> neighbor : neighbors){
-      int v = neighbor.first;
-      int alt = dist[u.first] + neighbor.second;
-      if(alt < dist[v]){
+    for (auto& [v, weight] : graph[u]) {
+      int alt = dist[u] + weight;
+      if (alt < dist[v]) {
         dist[v] = alt;
-        previous[v] = u.first;
-        q.push(make_pair(v, alt));
+        q.push({alt, v});
       }
     }
   }
 }
 
-void solve(){
+void solve() {
   int query;
   map<int, vector<pair<int, int>>> graph;
 
-  for(int i=0; i<edges; i++){
+  for (int i = 0; i < edges; i++) {
     int u, v, w;
     cin >> u >> v >> w;
 
-    graph[u].push_back(make_pair(v, w));
+    graph[u].push_back({v, w});
   }
 
   dijkstra(graph);
 
-  for(int i=0; i<queries; i++){
+  for (int i = 0; i < queries; i++) {
     cin >> query;
-    if(dist[query] == INF){
+    if (dist[query] == INF) {
       cout << "Impossible" << endl;
     } else {
       cout << dist[query] << endl;
@@ -59,9 +54,9 @@ void solve(){
   }
 }
 
-int main(){
-  while(scanf("%d %d %d %d", &nodes, &edges, &queries, &init_node) == 4){
-    if(nodes == 0 && edges == 0 && init_node == 0 && queries == 0) return 0;
+int main() {
+  while (scanf("%d %d %d %d", &nodes, &edges, &queries, &init_node) == 4) {
+    if (nodes == 0 && edges == 0 && init_node == 0 && queries == 0) return 0;
     solve();
   }
 }
