1bd9ca440fe473e51239dc599790869668557923
Chris Vilches <felovilches@gmail.com>
2022-02-16 21:16:12 +0900

    urionlinejudge/2904-building_a_field.go

---
+++ urionlinejudge/2904-building_a_field.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+var (
+	nums        [100001]int
+	N           int
+	totalLength int
+)
+
+func possible() bool {
+	partitions := 0
+	curr := nums[0]
+	j := 1
+
+	if totalLength%2 != 0 {
+		return false
+	}
+
+	half := totalLength / 2
+
+	for i := 0; i < N; i++ {
+		for ; j < N; j++ {
+			if curr == half {
+				partitions++
+				break
+			}
+
+			if curr >= half {
+				break
+			}
+
+			curr += nums[j]
+
+		}
+		curr -= nums[i]
+	}
+
+	return partitions >= 2
+}
+
+func main() {
+	fmt.Scan(&N)
+
+	totalLength = 0
+
+	for i := 0; i < N; i++ {
+		fmt.Scan(&nums[i])
+		totalLength += nums[i]
+	}
+
+	if possible() {
+		fmt.Println("Y")
+	} else {
+		fmt.Println("N")
+	}
+}
