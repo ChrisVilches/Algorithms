1c3c991e021f7abd88f38d11360b1811aab73924
Chris Vilches <felovilches@gmail.com>
2022-03-06 23:42:30 +0900

    refactor monkeys in the emei mountain

--- uva/11167-monkeys_in_the_emei_mountain.cpp
+++ uva/11167-monkeys_in_the_emei_mountain.cpp
@@ -2,131 +2,145 @@
 using namespace std;
 typedef pair<int, int> pii;
 
-#define V 1000
-int g[V][V], rgraph[V][V];
-bool visited[V];
-array<int, 50001> available;
-int N, M;
-const int S = 0;
-const int T = 1;
-
 struct Monkey {
   int v, a, b, node_idx;
 };
 
-struct Time {
-  Time(int t) : start(t) {}
+struct Period {
+  Period(int t) : start(t) {}
   int start, end = -1, node_idx = -1;
-  int amount() const { return end - start; }
+  int length() const { return end - start; }
 };
 
+int N, M;
+
 vector<Monkey> monkeys;
-map<int, Time> times;
-vector<vector<int>> edges;
+map<int, Period> periods;
 
-bool bfs(int rgraph[][V], int s, int t, array<int, V>& parent) {
-  memset(visited, 0, sizeof(visited));
-  queue<int> q;
-  q.push(s);
-  visited[s] = true;
-  parent[s] = -1;
+template <unsigned int V>
+struct FordFulkerson {
+  FordFulkerson(const vector<vector<pii>>& graph) {
+    original_graph = graph;
 
-  while (!q.empty()) {
-    int u = q.front();
-    q.pop();
+    memset(g, 0, sizeof g);
 
-    for (int v : edges[u]) {
-      if (!visited[v] && rgraph[u][v] > 0) {
-        if (v == t) {
-          parent[v] = u;
-          return true;
-        }
-        q.push(v);
-        parent[v] = u;
-        visited[v] = true;
+    for (int u = 0; u < (int)original_graph.size(); u++) {
+      for (const auto& [v, c] : original_graph[u]) {
+        g[u][v] = c;
       }
     }
   }
 
-  return false;
-}
+  int edge_flow(const int u, const int v) const { return g[u][v] - rgraph[u][v]; }
 
-int ford_fulkerson(int (&g)[V][V], int rgraph[][V], int s, int t) {
-  int u, v;
-  array<int, V> parent;
-  int max_flow = 0;
+  int calculate_flow(const int s, const int t) {
+    int u, v;
+    int max_flow = 0;
 
-  memcpy(rgraph, g, sizeof g);
+    memcpy(rgraph, g, sizeof g);
 
-  while (bfs(rgraph, s, t, parent)) {
-    int path_flow = INT_MAX;
-    for (v = t; v != s; v = parent[v]) {
-      u = parent[v];
-      path_flow = min(path_flow, rgraph[u][v]);
+    while (bfs(s, t)) {
+      int path_flow = INT_MAX;
+      for (v = t; v != s; v = parent[v]) {
+        u = parent[v];
+        path_flow = min(path_flow, rgraph[u][v]);
+      }
+
+      for (v = t; v != s; v = parent[v]) {
+        u = parent[v];
+        rgraph[u][v] -= path_flow;
+        rgraph[v][u] += path_flow;
+      }
+
+      max_flow += path_flow;
     }
 
-    for (v = t; v != s; v = parent[v]) {
-      u = parent[v];
-      rgraph[u][v] -= path_flow;
-      rgraph[v][u] += path_flow;
-    }
-
-    max_flow += path_flow;
+    return max_flow;
   }
 
-  return max_flow;
-}
+ private:
+  int g[V][V], rgraph[V][V], parent[V];
+  bool visited[V];
+  vector<vector<pii>> original_graph;
 
-void add_edge(int u, int v, int c) {
-  edges.at(u).push_back(v);
-  edges.at(v).push_back(u);
-  g[u][v] = c;
-}
+  bool bfs(const int s, const int t) {
+    memset(visited, 0, sizeof visited);
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    parent[s] = -1;
 
-void build_graph() {
-  times.clear();
+    while (!q.empty()) {
+      int u = q.front();
+      q.pop();
 
-  memset(g, 0, sizeof g);
+      for (auto [v, _] : original_graph[u]) {
+        if (!visited[v] && rgraph[u][v] > 0) {
+          if (v == t) {
+            parent[v] = u;
+            return true;
+          }
+          q.push(v);
+          parent[v] = u;
+          visited[v] = true;
+        }
+      }
+    }
 
-  edges.assign(2, vector<int>());
+    return false;
+  }
+};
+
+tuple<int, int, vector<vector<pii>>> build_graph() {
+  vector<vector<pii>> graph;
+
+  auto add_edge = [&](int u, int v, int c) -> void {
+    graph[u].push_back({v, c});
+    graph[v].push_back({u, 0});
+  };
+
+  auto add_node = [&]() -> int {
+    graph.push_back(vector<pii>());
+    return graph.size() - 1;
+  };
+
+  int source = add_node();
+  int sink = add_node();
 
   for (const Monkey& m : monkeys) {
-    times.emplace(m.a, m.a);
-    times.emplace(m.b, m.b);
+    periods.emplace(m.a, m.a);
+    periods.emplace(m.b, m.b);
   }
 
-  for (auto it = times.begin(); it != times.end(); it++) {
-    if (next(it) == times.end()) break;
+  for (auto it = periods.begin(); it != periods.end(); it++) {
+    if (next(it) == periods.end()) break;
 
-    Time& curr_time = it->second;
-    Time next_time = next(it)->second;
-    curr_time.end = next_time.start;
+    Period& curr_period = it->second;
+    Period next_period = next(it)->second;
+    curr_period.end = next_period.start;
   }
 
-  times.erase(prev(times.end()));
+  periods.erase(prev(periods.end()));
 
-  for (auto& [_, time] : times) {
-    edges.push_back(vector<int>());
-    time.node_idx = edges.size() - 1;
-  }
+  for (auto& [_, period] : periods) period.node_idx = add_node();
 
   for (Monkey& m : monkeys) {
-    edges.push_back(vector<int>());
-    const int monkey_node_idx = edges.size() - 1;
+    const int monkey_node_idx = add_node();
 
     for (int t = m.a; t < m.b;) {
-      const Time& time = times.at(t);
-      add_edge(monkey_node_idx, time.node_idx, time.amount());
-      t = time.end;
+      const Period& period = periods.at(t);
+      add_edge(monkey_node_idx, period.node_idx, period.length());
+      t = period.end;
     }
 
-    add_edge(S, monkey_node_idx, m.v);
+    add_edge(source, monkey_node_idx, m.v);
     m.node_idx = monkey_node_idx;
   }
 
-  for (const auto [_, time] : times) {
-    add_edge(time.node_idx, T, M * time.amount());
-  }
+  for (const auto [_, period] : periods)
+    add_edge(period.node_idx, sink, M * period.length());
+
+  return {source, sink, graph};
 }
 
 vector<pii> group_numbers(vector<int>& nums) {
@@ -143,27 +157,26 @@ vector<pii> group_numbers(vector<int>& nums) {
   return res;
 }
 
-int edge_flow(int u, int v) { return g[u][v] - rgraph[u][v]; }
-
-vector<vector<pii>> get_allocation() {
+vector<vector<pii>> get_allocation(FordFulkerson<1000>& ff) {
+  array<int, 50001> available;
   fill(available.begin(), available.end(), M);
 
   map<int, vector<int>> solutions;
 
-  for (const auto& [_, time] : times) {
+  for (const auto& [_, period] : periods) {
     set<pii, less<pii>> monkeys_to_allocate;
 
     for (const Monkey& monkey : monkeys) {
-      int flow = edge_flow(monkey.node_idx, time.node_idx);
+      int flow = ff.edge_flow(monkey.node_idx, period.node_idx);
 
       if (flow > 0) monkeys_to_allocate.emplace(flow, monkey.node_idx);
     }
 
-    int t = time.start;
+    int t = period.start;
 
     for (const auto& [flow, monkey_idx] : monkeys_to_allocate) {
       for (int f = 0; f < flow; t++) {
-        if (t == time.end) t = time.start;
+        if (t == period.end) t = period.start;
         if (available[t] == 0) continue;
 
         solutions[monkey_idx].push_back(t);
@@ -190,6 +203,7 @@ int main() {
     int total_required = 0;
 
     monkeys.clear();
+    periods.clear();
 
     for (int i = 0; i < N; i++) {
       Monkey m;
@@ -198,15 +212,17 @@ int main() {
       total_required += m.v;
     }
 
-    build_graph();
+    auto [source, sink, graph] = build_graph();
 
-    const int flow = ford_fulkerson(g, rgraph, S, T);
+    FordFulkerson<1000> ff(graph);
+
+    const int flow = ff.calculate_flow(source, sink);
 
     cout << "Case " << (test_case++) << ": " << (flow == total_required ? "Yes" : "No")
          << endl;
 
     if (flow == total_required) {
-      auto allocation = get_allocation();
+      auto allocation = get_allocation(ff);
 
       for (auto& alloc : allocation) {
         cout << alloc.size();
