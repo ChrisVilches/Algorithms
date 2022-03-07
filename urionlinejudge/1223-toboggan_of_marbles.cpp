bb090728a8fc0b0ed9fb56444238fa85b2f64117
Chris Vilches <felovilches@gmail.com>
2022-03-07 23:31:19 +0900

    refactor urionlinejudge/1223-toboggan_of_marbles.cpp

--- urionlinejudge/1223-toboggan_of_marbles.cpp
+++ urionlinejudge/1223-toboggan_of_marbles.cpp
@@ -10,48 +10,20 @@ struct Segment;
 
 struct Point {
   double x, y;
-  Point(double x, double y) : x(x), y(y) {}
-  Point() {}
-  Point operator+(const Point& p) { return Point(x + p.x, y + p.y); }
-  Point operator-(const Point& p) const { return Point(x - p.x, y - p.y); }
-  inline double operator^(const Point& p) const {
-    return (x * p.y) - (y * p.x);
-  }
-  Point scale(double f) { return Point(x * f, y * f); }
+  Point operator+(const Point& p) { return Point{x + p.x, y + p.y}; }
+  Point operator-(const Point& p) const { return Point{x - p.x, y - p.y}; }
+  Point scale(double f) { return Point{x * f, y * f}; }
   Point to(const Point& p) const { return p - *this; }
   double operator*(const Point& p) { return x * p.x + y * p.y; }
-  double dist(const Point& p) {
-    return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
-  }
+  double dist(const Point& p) { return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2)); }
   double dist(Segment& s);
 };
 
 struct Segment {
   Point p, q;
-  Segment(Point p, Point q) : p(p), q(q) {}
-  Segment() {}
 
   double dist(Segment& s) {
-    if (intersect(s)) return 0;
     return min({s.p.dist(*this), s.q.dist(*this), p.dist(s), q.dist(s)});
-  }
-
-  int orientation(Point p, Point q, Point r) {
-    int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-    if (val == 0) return 0;
-    return (val > 0) ? 1 : 2;
-  }
-
-  bool intersect(const Segment& s) {
-    Point p1 = p;
-    Point q1 = q;
-    Point p2 = s.p;
-    Point q2 = s.q;
-    int o1 = orientation(p1, q1, p2);
-    int o2 = orientation(p1, q1, q2);
-    int o3 = orientation(p2, q2, p1);
-    int o4 = orientation(p2, q2, q1);
-    return (o1 != o2 && o3 != o4);
   }
 };
 
@@ -74,13 +46,15 @@ void solve() {
   double L, H;
   cin >> L >> H;
 
-  vector<Segment> flaps;
+  vector<Segment> flaps(N);
 
   for (int i = 0; i < N; i++) {
     double yi, xf, yf;
     cin >> yi >> xf >> yf;
-    Point p(i % 2 == 0 ? 0 : L, yi), q(xf, yf);
-    flaps.push_back(Segment(p, q));
+    flaps[i].p.x = i % 2 == 0 ? 0 : L;
+    flaps[i].p.y = yi;
+    flaps[i].q.x = xf;
+    flaps[i].q.y = yf;
   }
 
   double ans = DBL_MAX;
