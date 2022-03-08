5ae5570580dba3481ed2f49ae7142345784a1ad7
Chris Vilches <felovilches@gmail.com>
2022-03-09 01:07:53 +0900

    refactor crisis at the wedding (using goto)

--- codeforces/crisis_at_the_wedding.cpp
+++ codeforces/crisis_at_the_wedding.cpp
@@ -2,49 +2,27 @@
 using namespace std;
 typedef long long ll;
 
-vector<ll> convert_start(const vector<ll>& nums) {
-  vector<ll> res;
-  const int N = nums.size();
-  int start = -1;
-
-  for (int i = 0; i < N; i++)
-    if (start == -1 || nums[i] > start) start = i;
-
-  for (int i = start, iter = N; iter--; i++) res.push_back(nums[i % N]);
-
-  return res;
-}
-
 ll cost(vector<ll> nums, const ll target) {
-  const int N = nums.size();
   ll cost = 0;
   ll surplus = 0;
 
-  nums = convert_start(nums);
+solve:
 
-  while (true) {
-    for (int i = 0; i < N; i++) {
-      ll& curr = nums[i];
+  for (ll& curr : nums) {
+    cost += surplus;
 
-      cost += surplus;
-
-      if (curr < target) {
-        const ll moved = min(surplus, target - curr);
-        curr += moved;
-        surplus -= moved;
-      } else if (curr > target) {
-        surplus += curr - target;
-        curr = target;
-      }
+    if (curr < target) {
+      const ll moved = min(surplus, target - curr);
+      curr += moved;
+      surplus -= moved;
+    } else if (curr > target) {
+      surplus += curr - target;
+      curr = target;
     }
-
-    bool ok = true;
-
-    for (int i = 0; i < N; i++)
-      if (nums[i] != target) ok = false;
-
-    if (ok) break;
   }
+
+  for (ll n : nums)
+    if (n != target) goto solve;
 
   return cost;
 }
