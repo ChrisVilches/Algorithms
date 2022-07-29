a7b13e636b48bd82783cb52540d4b3e86bdc31a2
Chris Vilches <felovilches@gmail.com>
2022-07-30 04:29:26 +0900

    landscape refactor

--- kattis/landscape.cpp
+++ kattis/landscape.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Delta {
-  int x, val, add;
-  Delta(const int x, const int val, const int add) : x(x), val(val), add(add) {}
-  bool operator<(const Delta& delta) const { return x < delta.x; }
-};
-
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
@@ -14,7 +8,7 @@ int main() {
   int N, K;
 
   while (cin >> N >> K) {
-    vector<Delta> deltas;
+    vector<pair<int, int>> deltas, slopes;
 
     for (int i = 0; i < K; i++) {
       char type;
@@ -23,37 +17,43 @@ int main() {
 
       switch (type) {
         case 'R':
-          deltas.emplace_back(x1, 1, 0);
-          deltas.emplace_back(x2 + 1, -1, 0);
+          deltas.emplace_back(x1, 1);
+          deltas.emplace_back(x2 + 1, -1);
           break;
         case 'D':
-          deltas.emplace_back(x1, -1, 0);
-          deltas.emplace_back(x2 + 1, 1, 0);
+          deltas.emplace_back(x1, -1);
+          deltas.emplace_back(x2 + 1, 1);
           break;
         default:
           const int a = type == 'V' ? 1 : -1;
           const int m = (x1 + x2) / 2;
 
-          deltas.emplace_back(x1, 0, -a);
-          deltas.emplace_back(1 + m, 0, a);
-          deltas.emplace_back(((x2 - x1) % 2) + 1 + m, 0, a);
-          deltas.emplace_back(x2 + 2, 0, -a);
+          slopes.emplace_back(x1, -a);
+          slopes.emplace_back(1 + m, a);
+          slopes.emplace_back(((x2 - x1) % 2) + 1 + m, a);
+          slopes.emplace_back(x2 + 2, -a);
       }
     }
 
     sort(deltas.begin(), deltas.end());
+    sort(slopes.begin(), slopes.end());
 
     long long curr = 0;
     long long add = 0;
 
     auto d = deltas.begin();
+    auto s = slopes.begin();
 
     for (int x = 1; x <= N; x++) {
-      while (d != deltas.end() && d->x == x) {
-        curr += d->val;
-        add += d->add;
+      while (d != deltas.end() && d->first == x) {
+        curr += d->second;
         ++d;
       }
+      while (s != slopes.end() && s->first == x) {
+        add += s->second;
+        ++s;
+      }
+
       curr += add;
       cout << curr << '\n';
     }
