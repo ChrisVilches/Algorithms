0bde8210a1dbef234ed158c9a0cd5dce44683015
Chris Vilches <felovilches@gmail.com>
2022-08-14 04:59:34 +0900

    refactor codechef/INSQ16G.cpp

--- codechef/INSQ16G.cpp
+++ codechef/INSQ16G.cpp
@@ -6,36 +6,26 @@ typedef long long ll;
 Weak test cases.
 AC with inefficient solution.
 Solution is most likely incorrect.
+
+Gets AC even without checking for this condition:
+"If you can't find regions such that all the points
+are the only corners and regions are closed, print 'Impossible'"
 */
 
 struct Point {
   int x, y;
+  bool operator<(const Point& p) const { return x < p.x || (x == p.x && y < p.y); }
+  Point rot() const { return {-y, x}; }
 };
-
-bool cmp_x(const Point& p, const Point& q) {
-  return p.x < q.x || (p.x == q.x && p.y < q.y);
-}
-
-bool cmp_y(const Point& p, const Point& q) {
-  return p.y < q.y || (p.y == q.y && p.x < q.x);
-}
 
 ll perimeter(vector<Point> points) {
   const int N = points.size();
   ll perimeter = 0;
 
-  sort(points.begin(), points.end(), cmp_x);
-  for (int i = 0; i < N; i += 2) {
-    const Point& p1 = points[i];
-    const Point& p2 = points[i + 1];
-    perimeter += abs(p1.y - p2.y);
-  }
-
-  sort(points.begin(), points.end(), cmp_y);
-  for (int i = 0; i < N; i += 2) {
-    const Point& p1 = points[i];
-    const Point& p2 = points[i + 1];
-    perimeter += abs(p1.x - p2.x);
+  for (int rot = 0; rot < 2; rot++) {
+    sort(points.begin(), points.end());
+    for (int i = 0; i < N; i += 2) perimeter += points[i + 1].y - points[i].y;
+    for (Point& p : points) p = p.rot();
   }
 
   return perimeter;
@@ -45,7 +35,7 @@ ll area_inefficient(vector<Point> points) {
   const int N = points.size();
   ll area = 0;
 
-  sort(points.begin(), points.end(), cmp_x);
+  sort(points.begin(), points.end());
 
   vector<int> active(1'000'001, -1);
 
@@ -73,11 +63,6 @@ int main() {
   while (cin >> N) {
     vector<Point> points(N);
     for (auto& p : points) cin >> p.x >> p.y;
-
-    if (points.size() % 2 == 1) {
-      cout << "impossible" << endl;
-      continue;
-    }
 
     cout << area_inefficient(points) << " " << perimeter(points) << endl;
   }
