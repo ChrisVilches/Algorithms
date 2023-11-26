d28900593999c936e22b441c4a8bc2c06b9ccee4
Chris Vilches <felovilches@gmail.com>
2023-11-26 13:16:45 +0900

    maxflow dinic

--- kattis/maxflow_dinic.cpp
+++ kattis/maxflow_dinic.cpp
@@ -1,59 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct FlowEdge {
-  int u, v;
-  long long cap, flow = 0;
-  FlowEdge(int u, int v, long long cap) : u(u), v(v), cap(cap) {}
-};
-
 struct Dinic {
-  vector<FlowEdge> edges;
+  Dinic(const int n) : graph(n), level(n), ptr(n) {}
 
-  Dinic(const int n, const int s, const int t) : n(n), s(s), t(t) {
-    adj.resize(n);
-    level.resize(n);
-    ptr.resize(n);
-  }
-
-  long long flow() {
+  long long max_flow(const int s, const int t) {
     long long f = 0;
-    while (true) {
-      fill(level.begin(), level.end(), -1);
-      level[s] = 0;
-      if (!bfs()) break;
+    while (bfs(s, t)) {
       fill(ptr.begin(), ptr.end(), 0);
-      while (long long pushed = dfs(s, flow_inf)) {
-        f += pushed;
-      }
+      while (const long long df = dfs(s, t, 1e18)) f += df;
     }
     return f;
   }
 
   void add_edge(const int u, const int v, const long long cap) {
-    edges.emplace_back(u, v, cap);
-    edges.emplace_back(v, u, 0);
-    adj[u].push_back(m++);
-    adj[v].push_back(m++);
+    const int r1 = graph[v].size();
+    const int r2 = graph[u].size();
+    graph[u].push_back({v, r1, cap});
+    graph[v].push_back({u, r2, 0});
+  }
+
+  vector<tuple<int, int, long long>> flow_edges() const {
+    vector<tuple<int, int, long long>> res;
+    for (int u = 0; u < (int)graph.size(); u++) {
+      for (const Edge edge : graph[u]) {
+        if (edge.flow > 0) {
+          res.emplace_back(u, edge.v, edge.flow);
+        }
+      }
+    }
+    return res;
   }
 
  private:
-  const long long flow_inf = 1e18;
-  vector<vector<int>> adj;
-  int n, m = 0;
-  int s, t;
-  vector<int> level, ptr;
-  queue<int> q;
+  struct Edge {
+    const int v, rev;
+    const long long cap;
+    long long flow = 0;
+  };
 
-  bool bfs() {
-    q.push(s);
+  vector<vector<Edge>> graph;
+  vector<int> level, ptr;
+
+  bool bfs(const int s, const int t) {
+    fill(level.begin(), level.end(), -1);
+    queue<int> q({s});
+    level[s] = 0;
     while (!q.empty()) {
       const int u = q.front();
       q.pop();
-      for (const int id : adj[u]) {
-        const FlowEdge edge = edges[id];
-        if (edge.cap - edge.flow < 1) continue;
-        if (level[edge.v] != -1) continue;
+      for (const Edge edge : graph[u]) {
+        if (level[edge.v] != -1 || edge.cap <= edge.flow) continue;
         level[edge.v] = level[u] + 1;
         q.push(edge.v);
       }
@@ -61,18 +58,16 @@ struct Dinic {
     return level[t] != -1;
   }
 
-  long long dfs(const int u, const long long pushed) {
-    if (pushed == 0) return 0;
-    if (u == t) return pushed;
-    for (int& cid = ptr[u]; cid < (int)adj[u].size(); cid++) {
-      const int id = adj[u][cid];
-      const int v = edges[id].v;
-      if (level[u] + 1 != level[v] || edges[id].cap - edges[id].flow < 1) continue;
-      const long long tr = dfs(v, min(pushed, edges[id].cap - edges[id].flow));
-      if (tr == 0) continue;
-      edges[id].flow += tr;
-      edges[id ^ 1].flow -= tr;
-      return tr;
+  long long dfs(const int u, const int t, const long long flow) {
+    if (u == t) return flow;
+    for (int& i = ptr[u]; i < (int)graph[u].size(); i++) {
+      Edge& edge = graph[u][i];
+      if (level[u] + 1 != level[edge.v] || edge.cap <= edge.flow) continue;
+      const long long f = dfs(edge.v, t, min(flow, edge.cap - edge.flow));
+      if (f == 0) continue;
+      edge.flow += f;
+      graph[edge.v][edge.rev].flow -= f;
+      return f;
     }
     return 0;
   }
@@ -82,23 +77,15 @@ int main() {
   int n, m, s, t;
 
   while (cin >> n >> m >> s >> t) {
-    Dinic dinic(n, s, t);
+    Dinic dinic(n);
     while (m--) {
       int u, v, c;
       cin >> u >> v >> c;
       dinic.add_edge(u, v, c);
     }
 
-    const long long flow = dinic.flow();
-
-    vector<tuple<int, int, long long>> edges;
-    for (const FlowEdge edge : dinic.edges) {
-      if (edge.flow > 0) {
-        edges.emplace_back(edge.u, edge.v, edge.flow);
-      }
-    }
-
-    sort(edges.begin(), edges.end());
+    const long long flow = dinic.max_flow(s, t);
+    const auto edges = dinic.flow_edges();
 
     cout << n << " " << flow << " " << edges.size() << endl;
 
