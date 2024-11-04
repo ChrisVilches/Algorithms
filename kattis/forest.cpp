2bf998e0515fda95ef3ff1c9678949b7976f0c34
Chris Vilches <felovilches@gmail.com>
2024-11-04 17:59:35 +0900

    refactor kattis/forest.cpp

--- kattis/forest.cpp
+++ kattis/forest.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define EPS 1e-7
 
 struct Point {
   double x, y;
@@ -11,82 +10,25 @@ struct Point {
   Point normalize() const { return {x / magnitude(), y / magnitude()}; }
   Point rot_ccw() const { return {-y, x}; }
   Point scale(const double f) const { return {x * f, y * f}; }
-  bool operator<(const Point& p) const {
-    return is_above() != p.is_above() ? is_above() : cross(p) > 0;
-  }
-  bool is_above() const { return y > 0 || (y == 0 && x > 0); }
 };
+
+int orientation(const Point o, const Point p, const Point q) {
+  const double x = (p - o).cross(q - o);
+  return (x > 0) - (x < 0);
+}
 
 struct Segment {
   Point p, q;
   Segment scale(const double f) const { return {p, p + (q - p).scale(f)}; }
 
-  pair<bool, Point> intersect_non_collinear(const Segment& s) const {
-    const bool o1 = orientation(p, q, s.p) * orientation(p, q, s.q) < 0;
-    const bool o2 = orientation(s.p, s.q, p) * orientation(s.p, s.q, q) < 0;
-    return o1 && o2 ? make_pair(true, intersection_point(s)) : make_pair(false, Point{});
-  }
-
- private:
-  int orientation(const Point& p, const Point& q, const Point& r) const {
-    double val = (q - p).cross(r - p);
-    if (fabs(val) < EPS) return 0;
-    return val > 0 ? 1 : -1;
-  }
-
-  Point intersection_point(const Segment& s) const {
-    const double factor = (s.q - s.p).cross(p - s.p) / (q - p).cross(s.q - s.p);
+  optional<Point> intersect_adhoc(const Segment s) const {
+    if (orientation(p, q, s.p) * orientation(p, q, s.q) == 1) return nullopt;
+    const double div = (q - p).cross(s.q - s.p);
+    if (div == 0) return nullopt;
+    const double factor = (s.q - s.p).cross(p - s.p) / div;
     return scale(factor).q;
   }
 };
-
-bool point_inside_polygon(const Point& p, const vector<Point>& polygon) {
-  const int n = polygon.size();
-  for (int i = 0; i < (int)polygon.size(); i++)
-    if ((polygon[(i + 1) % n] - polygon[i]).cross(p - polygon[i]) <= 0) return false;
-
-  return true;
-}
-
-Point point_set_centroid(const vector<Point>& points) {
-  Point centroid{0, 0};
-
-  for (const Point& p : points) {
-    centroid.x += p.x / points.size();
-    centroid.y += p.y / points.size();
-  }
-
-  return centroid;
-}
-
-vector<Point> sort_vertices(vector<Point> polygon_vertices) {
-  const Point centroid = point_set_centroid(polygon_vertices);
-  for (Point& p : polygon_vertices) p = p - centroid;
-  sort(polygon_vertices.begin(), polygon_vertices.end());
-  for (Point& p : polygon_vertices) p = p + centroid;
-  return polygon_vertices;
-}
-
-vector<Point> polygon_intersection(const vector<Point>& p1, const vector<Point>& p2) {
-  vector<Point> result;
-
-  for (const Point& p : p1)
-    if (point_inside_polygon(p, p2)) result.push_back(p);
-
-  for (const Point& p : p2)
-    if (point_inside_polygon(p, p1)) result.push_back(p);
-
-  for (int i = 0; i < (int)p1.size(); i++) {
-    for (int j = 0; j < (int)p2.size(); j++) {
-      const Segment edge1{p1[i], p1[(i + 1) % (int)p1.size()]};
-      const Segment edge2{p2[j], p2[(j + 1) % (int)p2.size()]};
-      const auto [intersect, point] = edge1.intersect_non_collinear(edge2);
-      if (intersect) result.push_back(point);
-    }
-  }
-
-  return sort_vertices(result);
-}
 
 double polygon_area(const vector<Point>& polygon) {
   double total = 0;
@@ -97,13 +39,40 @@ double polygon_area(const vector<Point>& polygon) {
   return fabs(total / 2);
 }
 
-vector<Point> build_line_polygon(const Point a, const Point b, const double d) {
-  const Point dir = (b - a).normalize();
-  const Point p = a + dir.scale(1e6);
-  const Point q = a - dir.scale(1e6);
-  const Point w = dir.rot_ccw().scale(d);
+vector<Point> intersection_adhoc(const Point a, const Point b, const double d,
+                                 const vector<Point>& polygon) {
+  vector<Point> res;
 
-  return {p + w, q + w, q - w, p - w};
+  const Point dir = (b - a).normalize();
+  const Point point1 = a + dir.rot_ccw().scale(d);
+  const Point point2 = a + dir.rot_ccw().scale(-d);
+  const array<Segment, 2> edges{
+      Segment{point1, point1 - dir},
+      Segment{point2, point2 + dir},
+  };
+
+  for (const Point p : polygon) {
+    if (all_of(edges.begin(), edges.end(),
+               [p](const auto e) { return orientation(e.p, e.q, p) == 1; })) {
+      res.emplace_back(p);
+    }
+  }
+
+  for (const Segment e : edges) {
+    const int n = polygon.size();
+    for (int i = n - 1, j = 0; j < n; i = j++) {
+      const optional<Point> point = e.intersect_adhoc({polygon[i], polygon[j]});
+      if (point.has_value()) res.emplace_back(point.value());
+    }
+  }
+
+  if (!res.empty()) {
+    sort(res.begin() + 1, res.end(), [&](const Point p, const Point q) {
+      return (p - res.front()).cross(q - res.front()) > 0;
+    });
+  }
+
+  return res;
 }
 
 int main() {
@@ -116,23 +85,19 @@ int main() {
     vector<Point> polygon(N);
     for (auto& p : polygon) cin >> p.x >> p.y;
     Point a, b;
-    cin >> a.x >> a.y;
-    cin >> b.x >> b.y;
-    cin >> d;
+    cin >> a.x >> a.y >> b.x >> b.y >> d;
     const Point o{0, 0};
-    vector<Point> line = build_line_polygon(a, b, d);
-    double ans = 0;
 
     const double total_area = polygon_area(polygon);
+    double ans = 0;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = N - 1, j = 0; j < N; i = j++) {
       const Point p = polygon[i];
-      const Point q = polygon[(i + 1) % N];
-      const double sign = p.cross(q) > 0 ? 1 : -1;
+      const Point q = polygon[j];
 
-      vector<Point> triangle{o, p, q};
-      vector<Point> intersection = polygon_intersection(line, triangle);
-      ans += sign * fabs(polygon_area(intersection));
+      const vector<Point> triangle{o, p, q};
+      const vector<Point> intersection = intersection_adhoc(a, b, d, triangle);
+      ans += orientation(o, p, q) * polygon_area(intersection);
     }
 
     cout << fixed << setprecision(9) << total_area - fabs(ans) << endl;
