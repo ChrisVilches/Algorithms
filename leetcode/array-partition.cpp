2637cd3c9bb3229e33806329225d0d33200bb4f0
Chris Vilches <felovilches@gmail.com>
2024-12-09 23:51:46 +0900

    leetcode/array-partition.cpp

---
+++ leetcode/array-partition.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int arrayPairSum(vector<int>& nums) {
+    const int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i += 2) {
+      ans += nums[i];
+    }
+
+    return ans;
+  }
+};
