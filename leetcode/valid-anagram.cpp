ee273294ec4ec00d63cc552fe2ba77dd35704308
Chris Vilches <felovilches@gmail.com>
2024-12-17 23:33:12 +0900

    leetcode/valid-anagram.cpp

---
+++ leetcode/valid-anagram.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  bool isAnagram(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    return s == t;
+  }
+};
