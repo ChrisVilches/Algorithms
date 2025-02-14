4438e3874b6092e078278e9838d491516d6f5cd8
Chris Vilches <felovilches@gmail.com>
2025-02-14 23:59:01 +0900

    leetcode/regular-expression-matching.cpp

---
+++ leetcode/regular-expression-matching.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  string s, p;
+  int memo[21][21];
+
+  int dp(const int i, const int j) {
+    if (i == (int)s.size() && j == (int)p.size()) return true;
+    if (~memo[i][j]) return memo[i][j];
+
+    bool res;
+
+    if (j < (int)p.size() - 1 && p[j + 1] == '*') {
+      if (i == (int)s.size()) {
+        res = dp(i, j + 2);
+      } else if (p[j] == '.') {
+        res = dp(i + 1, j) || dp(i, j + 2);
+      } else {
+        res = dp(i, j + 2) || (s[i] == p[j] && (dp(i + 1, j) || dp(i + 1, j + 2)));
+      }
+    } else {
+      if (i == (int)s.size()) {
+        res = false;
+      } else if (p[j] == '.') {
+        res = dp(i + 1, j + 1);
+      } else {
+        res = s[i] == p[j] && dp(i + 1, j + 1);
+      }
+    }
+
+    return memo[i][j] = res;
+  }
+
+ public:
+  bool isMatch(const string s, const string p) {
+    memset(memo, -1, sizeof memo);
+    this->s = s;
+    this->p = p;
+    return dp(0, 0);
+  }
+};
