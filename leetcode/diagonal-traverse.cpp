c1ca413a1743971b0363958327283614ac7f4fdf
Chris Vilches <felovilches@gmail.com>
2025-11-16 23:38:24 +0900

    leetcode/diagonal-traverse.cpp

---
+++ leetcode/diagonal-traverse.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findDiagonalOrder(const vector<vector<int>>& mat) {
+    const int n = mat.size();
+    const int m = mat.front().size();
+
+    vector<int> ans;
+
+    if (n == 1 || m == 1) {
+      for (const vector<int>& row : mat) {
+        for (const int x : row) {
+          ans.emplace_back(x);
+        }
+      }
+
+      return ans;
+    }
+
+    bool up = true;
+
+    int i = 0;
+    int j = 0;
+
+    while (static_cast<int>(ans.size()) < n * m) {
+      ans.emplace_back(mat[i][j]);
+
+      if (up) {
+        if (j == m - 1) {
+          i++;
+          up = false;
+        } else if (i == 0) {
+          j++;
+          up = false;
+        } else {
+          i--;
+          j++;
+        }
+      } else {
+        if (i == n - 1) {
+          j++;
+          up = true;
+        } else if (j == 0) {
+          i++;
+          up = true;
+        } else {
+          i++;
+          j--;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
