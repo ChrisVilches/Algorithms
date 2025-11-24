2d62ad3b73a2878805e480737bc6cca8b92e32fe
Chris Vilches <felovilches@gmail.com>
2025-11-25 02:45:52 +0900

    refactor leetcode/count-integers-in-intervals.cpp

--- leetcode/count-integers-in-intervals.cpp
+++ leetcode/count-integers-in-intervals.cpp
@@ -5,30 +5,13 @@ class CountIntervals {
   int cnt = 0;
   map<int, int> m;
 
-  map<int, int>::iterator find_prev(const int left) {
-    if (m.empty()) return m.end();
-
-    auto it = m.lower_bound(left);
-
-    if (it == m.end()) --it;
-
-    if (left < it->first) {
-      if (it != m.begin()) --it;
-    }
-
-    return it;
-  }
-
  public:
   void add(int left, int right) {
-    const auto prev_it = find_prev(left);
-    if (prev_it != m.end()) {
-      if (prev_it->first <= left) {
-        if (right <= prev_it->second) return;
-        if (left <= prev_it->second) {
-          left = prev_it->second + 1;
-        }
-      }
+    if (!m.empty()) {
+      const auto [a, b] = *prev(m.upper_bound(left));
+
+      if (a <= left && right <= b) return;
+      if (a <= left && left <= b) left = b + 1;
     }
 
     while (!m.empty()) {
